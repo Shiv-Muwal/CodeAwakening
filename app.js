52118134d4add4354e7da694ea6413019cfba2be
Shiv-Muwal <shivmuwal.147@gmail.com>
2025-07-04 19:54:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,13 +15,11 @@ import projectRouter from "./routes/projectRoutes.js";
 const app = express();
 dotenv.config({ path: "./config/config.env" });
 
-// Allowed origins for CORS
 const allowedOrigins = [
-  "https://codeawakening-dashboard-px5yl0h4g-shiv-468s-projects.vercel.app",
-  "https://codeawakening-dashboard.vercel.app",
+  process.env.PORTFOLIO_URL,
+  process.env.DASHBOARD_URL,
 ];
 
-// CORS middleware
 app.use(
   cors({
     origin: function (origin, callback) {
@@ -32,8 +30,10 @@ app.use(
       }
     },
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
+
 
 app.use(cookieParser());
 app.use(express.json());
